2adb54ba536f88245996c770da9654261597f739
Takumi Tsunokake <t.bekkou@gmail.com>
2012-04-04 13:25:16 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapBootcamp::Application.config.secret_token = '0726f3023e143b9d12e1ae34090cbed45145c8138947337239d6da9ddf083f6b0eedbeb50830918930e8323bf5935ad4c6f080a1d79cb880b4a06d9f60921de6'
